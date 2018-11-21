e36fbb6d2831de0196771385cf9366ce62656fe0
Alex Penson <pensona@mskcc.org>
2018-11-21 18:42:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 ## Make folder
 go_directory=$HOME/Go
-mkdir -p $go_directory && cd $go_directory
+mkdir -p $go_directory
 
 ## Install Dependencies
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install boost cmake wget gnu-sed
 
 ## Build Leela Zero
+cd $go_directory
 git clone --recursive --branch next http://github.com/gcp/leela-zero.git
 cd leela-zero
 
@@ -15,17 +16,18 @@ mkdir build && cd build
 cmake -DUSE_CPU_ONLY=1 ..
 cmake --build .
 ./tests
-cd ..
 
 ## Lizzie
+cd $go_directory
 wget https://github.com/featurecat/lizzie/releases/download/0.6/Lizzie.0.6.Mac-Linux.zip
 unzip Lizzie.0.6.Mac-Linux.zip
 rm Lizzie.0.6.Mac-Linux.zip
 cd Lizzie
 ln -s ../leela-zero/build/leelaz .
-cd ..
+
 
 ## Go Review Partner
+cd $go_directory
 wget https://github.com/pnprog/goreviewpartner/archive/v0.14.2.zip
 unzip v0.14.2.zip
 rm v0.14.2.zip
@@ -36,10 +38,11 @@ gsed -i \
     -e '89s|^.*$|fastcommand = '$go_directory'/leela-zero/build/leelaz|'\
     -e '90s|^.*$|fastparameters = --gtp --noponder --weights '$go_directory'/Lizzie/network.gz|' \
     config.ini
-cd ..
 
 ## test
+cd $go_directory
 ls
-mkdir -p Games/Pro && cd Games/Pro
+mkdir -p $go_directory/Games/Pro
+cd $go_directory/Games/Pro
 wget https://homepages.cwi.nl/\~aeb/go/games/games/AlphaGo/LeeSedol/1c.sgf
 python2.7 $go_directory/goreviewpartner-0.14.2/leela_zero_analysis.py --no-gui 1c.sgf
